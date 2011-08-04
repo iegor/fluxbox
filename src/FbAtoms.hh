fb8a7efdaa0eeead5507bf0011e88e5eb92f8128
Gediminas Liktaras <gliktaras@gmail.com>
2011-08-04 17:16:07 +0100

    Added a way to track the user in the iconbar.
    Added a new _FLUXBOX_* property that is set to the XID of the window
    that the button the user is hovering his mouse over manages.

--- src/FbAtoms.hh
+++ src/FbAtoms.hh
@@ -42,6 +42,7 @@ public:
     // these atoms are for normal app->WM interaction beyond the scope of the
     // ICCCM...
     Atom getFluxboxAttributesAtom() const { return blackbox_attributes; }
+    Atom getFluxboxCurrentIconbarItemAtom() const { return current_iconbar_item; }
     Atom getFluxboxResizeRectAtom() const { return resize_rect; }
 
 private:
@@ -56,6 +57,7 @@ private:
     Atom xa_wm_delete_window;
     Atom xa_wm_take_focus;
     Atom xa_wm_change_state;
+    Atom current_iconbar_item;
 };
 
 #endif //FBATOMS_HH
